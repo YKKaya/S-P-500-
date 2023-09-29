ee78820a9fda1bce87efaf3e89c3ecac872c7699
YKKaya <73110464+YKKaya@users.noreply.github.com>
2023-09-29 22:51:30 +0200

    Update SP500.py

--- SP500.py
+++ SP500.py
@@ -57,7 +57,7 @@ def download_link(object_to_download, download_filename, download_link_text):
 
 st.title("S&P 500 Analysis")
 st.write("""
-An interactive analysis of S&P 500 companies, allowing users to view and download historical stock data, returns,  additional company information. The dataset provides 1 year of historical data, recorded at hourly intervals. 
+An interactive analysis of S&P 500 companies, allowing users to view and download historical stock data, returns, additional company information. The dataset provides 1 year of historical data, recorded at hourly intervals. 
 """)
 
 
