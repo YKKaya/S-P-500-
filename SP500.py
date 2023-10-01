cbd9789db39f34dd61db11c9460fa7722d7c7a68
YKKaya <73110464+YKKaya@users.noreply.github.com>
2023-10-01 16:57:18 +0200

    Update SP500.py

--- SP500.py
+++ SP500.py
@@ -77,7 +77,7 @@ if portfolio is not None:
     symbol_data = filtered_portfolio[filtered_portfolio['Symbol'].isin(selected_symbols)]
 
     # Call the display_high_low function here
-    display_high_low(symbol_data, selected_symbols, start_date, end_date)
+     st.write(display_high_low(symbol_data, selected_symbols, start_date, end_date))
 
     # Now display the data table
     if 'Datetime' in symbol_data.columns:
