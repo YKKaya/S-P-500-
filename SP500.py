d65db53cc0c013adea9d3c25c8d9beb39ca24c6e
YKKaya <73110464+YKKaya@users.noreply.github.com>
2023-10-01 15:17:58 +0200

    Update SP500.py

--- SP500.py
+++ SP500.py
@@ -1,10 +1,10 @@
 # Importing required libraries
 import streamlit as st
 import pandas as pd
+import yfinance as yf
 import base64
 import io
 from datetime import datetime, timedelta
-import pandas_datareader as pdr
 
 # Function to fetch S&P 500 data
 @st.cache  # Adding caching here
@@ -20,8 +20,7 @@ def fetch_sp500_data(url):
 @st.cache  # Adding caching here
 def download_stock_data(Stocks):
     try:
-        # Use pandas_datareader to fetch the data
-        Portfolio = pdr.get_data_yahoo(Stocks, period='1y', interval='1h')
+        Portfolio = yf.download(Stocks, period='1y', interval='1h')
         return Portfolio
     except Exception as e:
         st.error(f"Error downloading stock data: {e}")
@@ -56,65 +55,54 @@ def download_link(object_to_download, download_filename, download_link_text):
     download_link = f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
     return download_link
 
-# Function to get the last weekday
-def last_weekday():
-    today = datetime.now()
-    offset = 1
-    while (today - timedelta(days=offset)).weekday() > 4:  # 0=Monday, 1=Tuesday, ..., 4=Friday
-        offset += 1
-    last_working_day = today - timedelta(days=offset)
-    return last_working_day
-
 st.title("S&P 500 Analysis")
 st.write("""
 An interactive analysis of S&P 500 companies, allowing users to view and download historical stock data, returns, 
 additional company information. The dataset provides 1 year of historical data, recorded at hourly intervals. 
 """)
 
+
 url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
 
 tickers = fetch_sp500_data(url)
 Stocks = tickers.Symbol.to_list()
 Portfolio = download_stock_data(Stocks)
+portfolio = process_data(Portfolio)
+portfolio = merge_additional_info(portfolio, tickers)
 
-if Portfolio is None:
-    st.error("Error in downloading stock data.")
-else:
-    portfolio = process_data(Portfolio)
-    portfolio = merge_additional_info(portfolio, tickers)
+if portfolio is not None:
+    portfolio['Founded'] = portfolio['Founded'].str.replace(r'\(.*?\)', '', regex=True).str.strip()
+    portfolio['Dollar_Return'] = portfolio['Return'] * portfolio['Adj Close']
 
-    if portfolio is not None:
-        portfolio['Founded'] = portfolio['Founded'].str.replace(r'\(.*?\)', '', regex=True).str.strip()
-        portfolio['Dollar_Return'] = portfolio['Return'] * portfolio['Adj Close']
+    # Date selection
+    yesterday = datetime.now() - timedelta(days=1)
+    selected_date = st.date_input("Select Date:", yesterday)
+    filtered_portfolio = portfolio[portfolio['Datetime'].dt.date == selected_date]
 
-        # Date selection
-        selected_date = st.date_input("Select Date:", last_weekday())
-        filtered_portfolio = portfolio[portfolio['Datetime'].dt.date == selected_date]
+    # Ticker selection
+    selected_symbol = st.selectbox("Ticker:", filtered_portfolio['Symbol'].unique())
 
-        # Ticker selection
-        selected_symbol = st.selectbox("Ticker:", filtered_portfolio['Symbol'].unique())
+    # Filter the data for the selected symbol
+    symbol_data = filtered_portfolio[filtered_portfolio['Symbol'] == selected_symbol]
 
-        # Filter the data for the selected symbol
-        symbol_data = filtered_portfolio[filtered_portfolio['Symbol'] == selected_symbol]
+    if symbol_data is not None and not symbol_data.empty:
+        if 'Datetime' in symbol_data.columns:
+            symbol_data.set_index('Datetime', inplace=True)
+            st.write("### Data Table:")
+            st.dataframe(symbol_data)
 
-        if symbol_data is not None and not symbol_data.empty:
-            if 'Datetime' in symbol_data.columns:
-                symbol_data.set_index('Datetime', inplace=True)
-                st.write("### Data Table:")
-                st.dataframe(symbol_data)
+            if st.button("Download data as CSV"):
+                tmp_download_link = download_link(symbol_data, 'your_data.csv', 'Click here to download your data as CSV!')
+                st.markdown(tmp_download_link, unsafe_allow_html=True)
 
-                if st.button("Download data as CSV"):
-                    tmp_download_link = download_link(symbol_data, 'your_data.csv', 'Click here to download your data as CSV!')
-                    st.markdown(tmp_download_link, unsafe_allow_html=True)
-
-                if st.button("Download data as Excel"):
-                    towrite = io.BytesIO()
-                    downloaded_file = symbol_data.to_excel(towrite, index=False, sheet_name='Sheet1')
-                    towrite.seek(0)
-                    b64 = base64.b64encode(towrite.read()).decode()
-                    tmp_download_link = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="your_data.xlsx">Download excel file</a>'
-                    st.markdown(tmp_download_link, unsafe_allow_html=True)
-            else:
-                st.error("Datetime column not found in the data.")
+            if st.button("Download data as Excel"):
+                towrite = io.BytesIO()
+                downloaded_file = symbol_data.to_excel(towrite, index=False, sheet_name='Sheet1')
+                towrite.seek(0)
+                b64 = base64.b64encode(towrite.read()).decode()
+                tmp_download_link = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="your_data.xlsx">Download excel file</a>'
+                st.markdown(tmp_download_link, unsafe_allow_html=True)
         else:
-            st.error("No data available for the selected symbol.")
+            st.error("Datetime column not found in the data.")
+    else:
+        st.error("No data available for the selected symbol.")
