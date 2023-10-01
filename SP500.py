2ffe87f4ad10652a870e51f1d9fda7bb839f65ab
YKKaya <73110464+YKKaya@users.noreply.github.com>
2023-10-01 18:01:56 +0200

    Update SP500.py

--- SP500.py
+++ SP500.py
@@ -55,11 +55,14 @@ def display_high_low(symbol_data, selected_symbols, start_date, end_date):
             min_return_row = single_symbol_data.loc[single_symbol_data['Low'].idxmin()]  # Get the row with the minimum 'Low' value
             max_return_row = single_symbol_data.loc[single_symbol_data['High'].idxmax()]  # Get the row with the maximum 'High' value
             
-            st.write(f"For the dates {start_date} to {end_date}, {symbol} recorded its:")
+            company_name = single_symbol_data['Company_Name'].values[0]  # Get the Company_Name from the dataset
+            
+            st.write(f"For the dates {start_date} to {end_date}, {company_name} recorded its:")
             st.write(f"Lowest trading price: ${min_return_row['Low']:.2f} on {min_return_row['Datetime'].strftime('%A, %B %d at %H:%M')}")
             st.write(f"Peak trading price: ${max_return_row['High']:.2f} on {max_return_row['Datetime'].strftime('%A, %B %d at %H:%M')}")
     except Exception as e:
         st.error(f"An error occurred: {e}")
+
 
 # Main part of the code
 st.title("S&P 500 Analysis")
