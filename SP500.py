c5b9c83bc585e4f00e1c66c32969722d5d2a79a1
YKKaya <73110464+YKKaya@users.noreply.github.com>
2023-10-01 16:16:31 +0200

    Update SP500.py

--- SP500.py
+++ SP500.py
@@ -36,19 +36,29 @@ def process_data(Portfolio):
         return None
 
 # Function to find highest and lowest return and corresponding times
-def find_high_low_return(symbol_data, selected_symbols):
-    if not symbol_data.empty:
-        highest_return_row = symbol_data[symbol_data['Return'] == symbol_data['Return'].max()]
-        lowest_return_row = symbol_data[symbol_data['Return'] == symbol_data['Return'].min()]
-        highest_time = highest_return_row.index[0].time()
-        highest_day = highest_return_row.index[0].strftime('%A')
-        lowest_time = lowest_return_row.index[0].time()
-        lowest_day = lowest_return_row.index[0].strftime('%A')
-        for symbol in selected_symbols:
-            text = f"{symbol} had its lowest trading price of its stock on {lowest_day} {lowest_time} and highest trading price of its stock at {highest_time} on {highest_day} for the selected dates."
-            st.write(text)
+def find_high_low_prices(symbol_data, selected_symbols, start_date, end_date):
+    if symbol_data.empty:
+        st.error("The symbol data is empty. Cannot find high and low prices.")
+        return
 
-# ... rest of the code remains unchanged
+    if 'High' not in symbol_data.columns or 'Low' not in symbol_data.columns:
+        st.error("The 'High' or 'Low' column is not found in the symbol data.")
+        return
+
+    try:
+        for symbol in selected_symbols:
+            single_symbol_data = symbol_data[symbol_data['Symbol'] == symbol]
+            single_symbol_data = single_symbol_data.loc[start_date:end_date]
+            highest_price_row = single_symbol_data[single_symbol_data['High'] == single_symbol_data['High'].max()]
+            lowest_price_row = single_symbol_data[single_symbol_data['Low'] == single_symbol_data['Low'].min()]
+            highest_price = highest_price_row['High'].values[0]
+            lowest_price = lowest_price_row['Low'].values[0]
+            highest_datetime = highest_price_row.index[0]
+            lowest_datetime = lowest_price_row.index[0]
+            text = f"{symbol} had its lowest trading price of ${lowest_price:.2f} on {lowest_datetime.strftime('%A, %B %d, %Y at %H:%M')} and highest trading price of ${highest_price:.2f} on {highest_datetime.strftime('%A, %B %d, %Y at %H:%M')} for the selected dates of {start_date} to {end_date}."
+            st.write(text)
+    except Exception as e:
+        st.error(f"An error occurred while finding high and low prices: {e}")
 
 st.title("S&P 500 Analysis")
 st.write("""
@@ -91,8 +101,8 @@ if portfolio is not None:
     # Filter the data for the selected symbols
     symbol_data = filtered_portfolio[filtered_portfolio['Symbol'].isin(selected_symbols)]
 
-    # Call the function to find highest and lowest return and display the text
-    find_high_low_return(symbol_data, selected_symbols)
+   # After filtering the symbol_data DataFrame based on the selected symbols and date range, call the function:
+    find_high_low_prices(symbol_data, selected_symbols, start_date, end_date)
 
     if not symbol_data.empty:
         st.write("### Data Table:")
