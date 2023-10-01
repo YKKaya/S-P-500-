a8f88bad0df85eeb4434d044967a9d6d7fe59236
YKKaya <73110464+YKKaya@users.noreply.github.com>
2023-10-01 16:21:07 +0200

    Update SP500.py

--- SP500.py
+++ SP500.py
@@ -6,7 +6,7 @@ import io
 from datetime import datetime, timedelta
 
 # Function to fetch S&P 500 data
-@st.cache
+@st.cache  # Adding caching here
 def fetch_sp500_data(url):
     try:
         tickers = pd.read_html(url)[0]
@@ -16,7 +16,7 @@ def fetch_sp500_data(url):
         return None
 
 # Function to download stock data
-@st.cache
+@st.cache  # Adding caching here
 def download_stock_data(Stocks):
     try:
         Portfolio = yf.download(Stocks, period='1y', interval='1h')
@@ -35,30 +35,15 @@ def process_data(Portfolio):
         st.error(f"Error processing data: {e}")
         return None
 
-# Function to find highest and lowest return and corresponding times
-def find_high_low_prices(symbol_data, selected_symbols, start_date, end_date):
-    if symbol_data.empty:
-        st.error("The symbol data is empty. Cannot find high and low prices.")
-        return
-
-    if 'High' not in symbol_data.columns or 'Low' not in symbol_data.columns:
-        st.error("The 'High' or 'Low' column is not found in the symbol data.")
-        return
-
-    try:
-        for symbol in selected_symbols:
-            single_symbol_data = symbol_data[symbol_data['Symbol'] == symbol]
-            single_symbol_data = single_symbol_data.loc[start_date:end_date]
-            highest_price_row = single_symbol_data[single_symbol_data['High'] == single_symbol_data['High'].max()]
-            lowest_price_row = single_symbol_data[single_symbol_data['Low'] == single_symbol_data['Low'].min()]
-            highest_price = highest_price_row['High'].values[0]
-            lowest_price = lowest_price_row['Low'].values[0]
-            highest_datetime = highest_price_row.index[0]
-            lowest_datetime = lowest_price_row.index[0]
-            text = f"{symbol} had its lowest trading price of ${lowest_price:.2f} on {lowest_datetime.strftime('%A, %B %d, %Y at %H:%M')} and highest trading price of ${highest_price:.2f} on {highest_datetime.strftime('%A, %B %d, %Y at %H:%M')} for the selected dates of {start_date} to {end_date}."
-            st.write(text)
-    except Exception as e:
-        st.error(f"An error occurred while finding high and low prices: {e}")
+# Function to get the last weekday
+def last_weekday():
+    today = datetime.now()
+    offset = 1
+    while (today - timedelta(days=offset)).weekday() > 4:  # 0=Monday, 1=Tuesday, ..., 4=Friday
+        offset += 1
+    last_working_day = today - timedelta(days=offset)
+    st.write(f"Last Working Day: {last_working_day}")  # Add this line for debugging
+    return last_working_day
 
 st.title("S&P 500 Analysis")
 st.write("""
@@ -74,50 +59,50 @@ Portfolio = download_stock_data(Stocks)
 portfolio = process_data(Portfolio)
 
 if portfolio is not None:
-    portfolio['Datetime'] = pd.to_datetime(portfolio['Datetime'])
-    portfolio.set_index('Datetime', inplace=True)
+    portfolio['Founded'] = portfolio['Founded'].str.replace(r'\(.*?\)', '', regex=True).str.strip()
     portfolio['Dollar_Return'] = portfolio['Return'] * portfolio['Adj Close']
 
     # Date range selection
     st.write("Select Date Range:")
     start_date = st.date_input(
         "Start Date",
-        value=last_weekday() - timedelta(days=30),
-        min_value=datetime.now() - timedelta(days=365),
-        max_value=last_weekday(),
+        value=last_weekday() - timedelta(days=30),  # Default value is 30 days ago
+        min_value=datetime.now() - timedelta(days=365),  # Min value is one year ago
+        max_value=last_weekday(),  # Max value is the last working day
     )
     end_date = st.date_input(
         "End Date",
-        value=last_weekday(),
-        min_value=datetime.now() - timedelta(days=365),
-        max_value=last_weekday(),
+        value=last_weekday(),  # Default value is the last working day
+        min_value=datetime.now() - timedelta(days=365),  # Min value is one year ago
+        max_value=last_weekday(),  # Max value is the last working day
     )
 
-    filtered_portfolio = portfolio.loc[start_date:end_date]
+    filtered_portfolio = portfolio[(portfolio['Datetime'].dt.date >= start_date) & (portfolio['Datetime'].dt.date <= end_date)]
 
     # Ticker selection
     selected_symbols = st.multiselect("Tickers:", filtered_portfolio['Symbol'].unique(), default=['AAPL'])
 
     # Filter the data for the selected symbols
     symbol_data = filtered_portfolio[filtered_portfolio['Symbol'].isin(selected_symbols)]
+    
+    if symbol_data is not None and not symbol_data.empty:
+        if 'Datetime' in symbol_data.columns:
+            symbol_data.set_index('Datetime', inplace=True)
+            st.write("### Data Table:")
+            st.dataframe(symbol_data)
 
-   # After filtering the symbol_data DataFrame based on the selected symbols and date range, call the function:
-    find_high_low_prices(symbol_data, selected_symbols, start_date, end_date)
+            if st.button("Download data as CSV"):
+                tmp_download_link = download_link(symbol_data, 'your_data.csv', 'Click here to download your data as CSV!')
+                st.markdown(tmp_download_link, unsafe_allow_html=True)
 
-    if not symbol_data.empty:
-        st.write("### Data Table:")
-        st.dataframe(symbol_data)
-
-        if st.button("Download data as CSV"):
-            tmp_download_link = download_link(symbol_data, 'your_data.csv', 'Click here to download your data as CSV!')
-            st.markdown(tmp_download_link, unsafe_allow_html=True)
-
-        if st.button("Download data as Excel"):
-            towrite = io.BytesIO()
-            downloaded_file = symbol_data.to_excel(towrite, index=True, sheet_name='Sheet1')
-            towrite.seek(0)
-            b64 = base64.b64encode(towrite.read()).decode()
-            tmp_download_link = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="your_data.xlsx">Download excel file</a>'
-            st.markdown(tmp_download_link, unsafe_allow_html=True)
+            if st.button("Download data as Excel"):
+                towrite = io.BytesIO()
+                downloaded_file = symbol_data.to_excel(towrite, index=False, sheet_name='Sheet1')
+                towrite.seek(0)
+                b64 = base64.b64encode(towrite.read()).decode()
+                tmp_download_link = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="your_data.xlsx">Download excel file</a>'
+                st.markdown(tmp_download_link, unsafe_allow_html=True)
+        else:
+            st.error("Datetime column not found in the data.")
     else:
         st.error("No data available for the selected symbol.")
