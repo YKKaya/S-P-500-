afae0efd413043055aa9b55991d7944dde282577
YKKaya <73110464+YKKaya@users.noreply.github.com>
2023-10-01 19:06:54 +0200

    Update SP500.py

--- SP500.py
+++ SP500.py
@@ -59,7 +59,7 @@ def display_high_low(symbol_data, selected_symbols, start_date, end_date):
             
             company_name = single_symbol_data['Company_Name'].values[0]  # Get the Company_Name from the dataset
             
-            st.write(f"For the dates {start_date} to {end_date}, {company_name} recorded its:") & (f"Lowest trading price: ${min_return_row['Low']:.2f} on {min_return_row['Datetime'].strftime('%A, %B %d at %H:%M')}")
+            st.write(f"For the dates {start_date} to {end_date}, {company_name} recorded its:") & st.write(f"Lowest trading price: ${min_return_row['Low']:.2f} on {min_return_row['Datetime'].strftime('%A, %B %d at %H:%M')}")
             st.write(f"Lowest trading price: ${min_return_row['Low']:.2f} on {min_return_row['Datetime'].strftime('%A, %B %d at %H:%M')}")
             st.write(f"Highest trading price: ${max_return_row['High']:.2f} on {max_return_row['Datetime'].strftime('%A, %B %d at %H:%M')}")
     except Exception as e:
