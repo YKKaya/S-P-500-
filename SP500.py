ac71e9f21e02e3c06d6020c30008d21d51f46578
YKKaya <73110464+YKKaya@users.noreply.github.com>
2023-10-01 14:48:27 +0200

    Update SP500.py

--- SP500.py
+++ SP500.py
@@ -2,9 +2,6 @@
 import streamlit as st
 import pandas as pd
 import yfinance as yf
-import base64
-import io
-from datetime import datetime, timedelta
 
 # Function to fetch S&P 500 data
 @st.cache  # Adding caching here
@@ -21,8 +18,6 @@ def fetch_sp500_data(url):
 def download_stock_data(Stocks):
     try:
         Portfolio = yf.download(Stocks, period='1y', interval='1h')
-        Portfolio.index = pd.to_datetime(Portfolio.index)  # Convert index to datetime
-        Portfolio.index = Portfolio.index.tz_localize('UTC')  # Localize to UTC
         return Portfolio
     except Exception as e:
         st.error(f"Error downloading stock data: {e}")
@@ -48,63 +43,66 @@ def merge_additional_info(portfolio, tickers):
     except Exception as e:
         return None
 
-# Function to download data as csv
-def download_link(object_to_download, download_filename, download_link_text):
-    if isinstance(object_to_download, pd.DataFrame):
-        object_to_download = object_to_download.to_csv(index=False)
-
-    b64 = base64.b64encode(object_to_download.encode()).decode()
-    download_link = f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
-    return download_link
-
 st.title("S&P 500 Analysis")
 st.write("""
-An interactive analysis of S&P 500 companies, allowing users to view and download historical stock data, returns, 
-additional company information. The dataset provides 1 year of historical data, recorded at hourly intervals. 
+An interactive analysis of S&P 500 companies, allowing users to view historical stock data, returns, and additional company information.
 """)
 
 
 url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
 
+# Fetching the list of S&P 500 companies from Wikipedia
 tickers = fetch_sp500_data(url)
+
+# Selecting the ticker symbols
 Stocks = tickers.Symbol.to_list()
+
+# Downloading historical stock data
 Portfolio = download_stock_data(Stocks)
+
+# Processing the stock data
 portfolio = process_data(Portfolio)
+
+# Merging additional company information
 portfolio = merge_additional_info(portfolio, tickers)
 
+# Cleaning the 'Founded' column by removing year values wrapped in parentheses
 if portfolio is not None:
     portfolio['Founded'] = portfolio['Founded'].str.replace(r'\(.*?\)', '', regex=True).str.strip()
-    portfolio['Dollar_Return'] = portfolio['Return'] * portfolio['Adj Close']
 
-    # Date selection
-    yesterday = datetime.now() - timedelta(days=1)
-    selected_date = st.date_input("Select Date:", yesterday)
-    filtered_portfolio = portfolio[portfolio['Datetime'].dt.date == selected_date]
+# Calculating the dollar value of the return
+portfolio['Dollar_Return'] = portfolio['Return'] * portfolio['Adj Close']
 
-    # Ticker selection
+# Display Results
+st.write("### Data Table:")
+# Display the processed data in a table format
+st.dataframe(portfolio)
+
+
+# Rolling Visualization
+st.write("### Rolling Visualization:")
+
+# After filtering the portfolio based on the selected date
+selected_date = st.date_input("Select Date:", max_value=pd.to_datetime('today'))
+filtered_portfolio = portfolio[portfolio['Datetime'].dt.date == selected_date]
+
+# Check if filtered_portfolio is not None, not empty, and contains 'Symbol' column
+if filtered_portfolio is not None and 'Symbol' in filtered_portfolio.columns and not filtered_portfolio.empty:
     selected_symbol = st.selectbox("Ticker:", filtered_portfolio['Symbol'].unique())
+    # ... Continue with the rest of your code
+else:
+    st.error("No data available for the selected date.")
+    
+symbol_data.set_index('Datetime', inplace=True)
+symbol_data['Close'].plot()
+plt.title(f"Rolling Visualization for {selected_symbol}")
+plt.xlabel("Datetime")
+plt.ylabel("Close Price")
+st.pyplot(plt)
 
-    # Filter the data for the selected symbol
-    symbol_data = filtered_portfolio[filtered_portfolio['Symbol'] == selected_symbol]
-
-    if symbol_data is not None and not symbol_data.empty:
-        if 'Datetime' in symbol_data.columns:
-            symbol_data.set_index('Datetime', inplace=True)
-            st.write("### Data Table:")
-            st.dataframe(symbol_data)
-
-            if st.button("Download data as CSV"):
-                tmp_download_link = download_link(symbol_data, 'your_data.csv', 'Click here to download your data as CSV!')
-                st.markdown(tmp_download_link, unsafe_allow_html=True)
-
-            if st.button("Download data as Excel"):
-                towrite = io.BytesIO()
-                downloaded_file = symbol_data.to_excel(towrite, index=False, sheet_name='Sheet1')
-                towrite.seek(0)
-                b64 = base64.b64encode(towrite.read()).decode()
-                tmp_download_link = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="your_data.xlsx">Download excel file</a>'
-                st.markdown(tmp_download_link, unsafe_allow_html=True)
-        else:
-            st.error("Datetime column not found in the data.")
-    else:
-        st.error("No data available for the selected symbol.")
+# Download the data table
+if st.button("Download Data Table"):
+    csv = filtered_portfolio.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="filtered_data.csv">Download CSV File</a>'
+    st.markdown(href, unsafe_allow_html=True)
