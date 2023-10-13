1399bb618a2fe01bcad655933f83da7990021f4e
YKKaya <73110464+YKKaya@users.noreply.github.com>
2023-10-13 14:04:11 +0200

    Update SP500.py

--- SP500.py
+++ SP500.py
@@ -2,13 +2,11 @@ import streamlit as st
 import pandas as pd
 import yfinance as yf
 from datetime import datetime, timedelta
-import plotly.express as px
-import plotly.graph_objects as go
 import requests
 from bs4 import BeautifulSoup
 
 # Function to fetch S&P 500 data
-@st.cache
+@st.cache_data
 def fetch_sp500_data(url):
     try:
         tickers = pd.read_html(url)[0]
@@ -18,98 +16,59 @@ def fetch_sp500_data(url):
         return None
 
 # Function to download stock data
-@st.cache
+@st.cache_data
 def download_stock_data(Stocks):
     try:
         Portfolio = yf.download(Stocks, period='1y', interval='1h')
+        Portfolio = Portfolio.stack(level=0).rename_axis(['Date', 'Symbol']).reset_index(level=1)
         return Portfolio
     except Exception as e:
         st.error(f"Error downloading stock data: {e}")
         return None
-        
+
 # Function to extract ESG
-@st.cache
+@st.cache_data
 def get_esg_data_with_headers_and_error_handling(ticker):
-    url = f"https://uk.finance.yahoo.com/quote/{ticker}/sustainability?p={ticker}"
-    response = requests.get(url, headers=headers)
-
-    if response.status_code != 200:
-        print(f"Failed to fetch data for {ticker}. Status code: {response.status_code}")
-        return None
-
-    soup = BeautifulSoup(response.content, 'html.parser')
-    result = {}
-
-    try:
-        total_esg_risk_score = soup.find("div", {"class": "Fz(36px) Fw(600) D(ib) Mend(5px)"}).text
-        result["Total ESG risk score"] = float(total_esg_risk_score)
-    except:
-        result["Total ESG risk score"] = None
-
-    try:
-        nth_percentile = soup.find("div", {"class": "D(ib) Fz(12px) Fw(n) Mstart(2px)"}).text
-        result["n'th percentile"] = nth_percentile
-    except:
-        result["n'th percentile"] = None
-
-    scores = soup.find_all("div", {"class": "D(ib) Fz(23px) smartphone_Fz(22px) Fw(600)"})
-    try:
-        result["Environment risk score"] = float(scores[0].text)
-    except:
-        result["Environment risk score"] = None
-
-    try:
-        result["Social risk score"] = float(scores[1].text)
-    except:
-        result["Social risk score"] = None
-
-    try:
-        result["Governance risk score"] = float(scores[2].text)
-    except:
-        result["Governance risk score"] = None
-
-    try:
-        controversy_level = soup.find("div", {"class": "D(ib) Fz(36px) Fw(500)"}).text
-        result["Controversy level"] = int(controversy_level)
-    except:
-        result["Controversy level"] = None
-
-    return result       
+    # ... [same code as before] ...
 
 # Main part of the code
-st.title("S&P 500 Companies Hourly Returns")
-st.write("""
-An interactive analysis of S&P 500 companies, allowing users to view and download historical stock data, returns, 
-additional company information. The dataset provides 1 year of historical data, recorded at hourly intervals. 
-""")
+st.title("S&P 500 Companies Hourly Returns and ESG Data")
+
+# ESG Data Retrieval
+st.write("### Retrieve ESG Data")
+user_input_ticker = st.text_input("Enter a ticker symbol for ESG data (e.g., AAPL):")
+if user_input_ticker:
+    esg_data = get_esg_data_with_headers_and_error_handling(user_input_ticker)
+    if esg_data:
+        st.write(f"### ESG Data for {user_input_ticker}:")
+        st.write(esg_data)
+    else:
+        st.write(f"No ESG data available for {user_input_ticker}.")
 
 url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
 tickers = fetch_sp500_data(url)
 Stocks = tickers.Symbol.to_list()
 Portfolio = download_stock_data(Stocks)
 
-if Portfolio is not None:
-    # Date range selection
-    st.write("Select Date Range:")
-    start_date = st.date_input("Start Date", value=datetime.now() - timedelta(days=30), max_value=datetime.now())
-    end_date = st.date_input("End Date", value=datetime.now(), max_value=datetime.now())
-    filtered_portfolio = Portfolio[(Portfolio['Datetime'].dt.date >= start_date) & (Portfolio['Datetime'].dt.date <= end_date)]
+# Date range selection
+st.write("Select Date Range:")
+start_date = st.date_input("Start Date", value=datetime.now() - timedelta(days=30), max_value=datetime.now())
+end_date = st.date_input("End Date", value=datetime.now(), max_value=datetime.now())
+filtered_portfolio = Portfolio[(Portfolio['Date'].dt.date >= start_date) & (Portfolio['Date'].dt.date <= end_date)]
 
-    # Ticker selection
-    default_ticker = ['AAPL']
-    selected_symbols = st.multiselect("Tickers:", filtered_portfolio['Symbol'].unique(), default=default_ticker)
-    
-    # ESG selection
-    for symbol in selected_symbols:
-        esg_data = get_esg_data_with_headers_and_error_handling(symbol)
-        if esg_data:
-            st.write(f"### ESG Data for {symbol}:")
-            st.write(esg_data)
-        else:
-            st.write(f"No ESG data available for {symbol}.")
-        
-    # Filter the data for the selected symbols
-    symbol_data = filtered_portfolio[filtered_portfolio['Symbol'].isin(selected_symbols)]
+# Ticker selection
+default_ticker = ['AAPL']
+selected_symbols = st.multiselect("Tickers:", filtered_portfolio['Symbol'].unique(), default=default_ticker)
+
+# Filter the data for the selected symbols
+symbol_data = filtered_portfolio[filtered_portfolio['Symbol'].isin(selected_symbols)]
+
+# Display the time series chart for selected tickers
+if selected_symbols:  # Check if at least one ticker is selected
+    # ... [rest of your Streamlit app code here] ...
+else:
+    st.warning("Please select at least one ticker for comparison.")
+
 
     # Display the time series chart for selected tickers
     if selected_symbols:  # Check if at least one ticker is selected
