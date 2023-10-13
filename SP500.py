13bdb3c4eab2352ae45d57555237712eb30be743
YKKaya <73110464+YKKaya@users.noreply.github.com>
2023-10-13 17:30:15 +0200

    Update SP500.py

--- SP500.py
+++ SP500.py
@@ -2,8 +2,11 @@ import streamlit as st
 import pandas as pd
 import yfinance as yf
 from datetime import datetime, timedelta
-import plotly.express as px
+import requests
+from bs4 import BeautifulSoup
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import plotly.express as px
 
 # Function to fetch S&P 500 data
 @st.cache
@@ -24,7 +27,66 @@ def download_stock_data(Stocks):
     except Exception as e:
         st.error(f"Error downloading stock data: {e}")
         return None
+        
+# Function to extract esg data        
+@st.cache
+def get_esg_data_with_headers_and_error_handling(ticker):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+    url = f"https://uk.finance.yahoo.com/quote/{ticker}/sustainability?p={ticker}"
+    response = requests.get(url, headers=headers)
 
+    if response.status_code != 200:
+        print(f"Failed to fetch data for {ticker}. Status code: {response.status_code}")
+        return None
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    result = {}
+
+    try:
+        total_esg_risk_score = soup.find("div", {"class": "Fz(36px) Fw(600) D(ib) Mend(5px)"}).text
+        result["Total ESG risk score"] = float(total_esg_risk_score)
+    except:
+        result["Total ESG risk score"] = None
+
+    scores = soup.find_all("div", {"class": "D(ib) Fz(23px) smartphone_Fz(22px) Fw(600)"})
+    try:
+        result["Environment risk score"] = float(scores[0].text)
+    except:
+        result["Environment risk score"] = None
+
+    try:
+        result["Social risk score"] = float(scores[1].text)
+    except:
+        result["Social risk score"] = None
+
+    try:
+        result["Governance risk score"] = float(scores[2].text)
+    except:
+        result["Governance risk score"] = None
+
+    try:
+        controversy_level = soup.find("div", {"class": "D(ib) Fz(36px) Fw(500)"}).text
+        result["Controversy level"] = int(controversy_level)
+    except:
+        result["Controversy level"] = None
+
+    return result
+
+# Function to map ESG risk score to risk level
+def map_esg_risk_to_level(score):
+    if score < 10:
+        return "Very Low"
+    elif 10 <= score < 20:
+        return "Low"
+    elif 20 <= score < 30:
+        return "Medium"
+    elif 30 <= score < 40:
+        return "High"
+    else:
+        return "Severe"
+        
 # Function to process data
 def process_data(Portfolio):
     try:
@@ -34,6 +96,60 @@ def process_data(Portfolio):
     except Exception as e:
         st.error(f"Error processing data: {e}")
         return None
+        
+# Function to display consolidated ESG data in a table format
+def display_esg_data_table(selected_symbols, esg_data_list):
+    # Convert the list of dictionaries to a DataFrame
+    esg_df = pd.DataFrame(esg_data_list)
+    esg_df.insert(0, 'Ticker', selected_symbols)  # Add a column for tickers at the beginning
+    st.write("### ESG Data Table:")
+    st.table(esg_df)
+        
+# Function to display ESG risk levels
+def display_risk_levels(tickers, esg_scores):
+    st.write("### ESG Risk Levels:")
+    
+    risk_levels = ["Very Low", "Low", "Medium", "High", "Severe"]
+    score_ranges = [5, 15, 25, 35, 45]  # Midpoint of each score range for plotting
+    colors = ["#FFEDCC", "#FFDB99", "#FFC266", "#FF9900", "#FF6600"]  # Shades of orange from light to dark
+    
+    # Create a DataFrame for plotting
+    df = pd.DataFrame({
+        'Risk Level': risk_levels,
+        'Score Range': score_ranges,
+        'Color': colors
+    })
+    
+    # Create a bar chart
+    fig = px.bar(df, x='Score Range', y='Risk Level', color='Color', orientation='h',
+                 color_discrete_map=dict(zip(df['Color'], df['Color'])))
+    
+    # Annotate the chart with the scores of all selected tickers
+    for ticker, score in zip(tickers, esg_scores):
+        score_position = risk_levels.index(map_esg_risk_to_level(score))
+        annotation_x = df.loc[score_position, 'Score Range'] - 3  # Adjusted for visibility
+        fig.add_annotation(
+            x=annotation_x,
+            y=risk_levels[score_position],
+            text=f"{ticker}: {score}",
+            showarrow=False,
+            font=dict(color='black', size=12),
+            xshift=10
+        )
+    
+    # Update chart aesthetics
+    fig.update_layout(
+        title="ESG Risk Levels and Ticker Scores",
+        xaxis_title="Score Range",
+        yaxis_title="Risk Level",
+        showlegend=False,
+        plot_bgcolor='#2e2e2e',
+        paper_bgcolor='#2e2e2e',
+        font=dict(color='white')
+    )
+    
+    st.plotly_chart(fig)
+
 
 # Function to merge additional info
 def merge_additional_info(portfolio, tickers):
@@ -44,27 +160,6 @@ def merge_additional_info(portfolio, tickers):
         return portfolio
     except Exception as e:
         return None
-        
-# Function to display high and low return text
-def display_high_low(symbol_data, selected_symbols, start_date, end_date):
-    try:
-        for symbol in selected_symbols:
-            single_symbol_data = symbol_data[symbol_data['Symbol'] == symbol]
-            single_symbol_data['Datetime'] = pd.to_datetime(single_symbol_data['Datetime'])  # Ensure Datetime is in datetime format
-            if single_symbol_data.empty:
-                st.error(f"No data available for {symbol} in the selected date range.")
-                continue
-            min_return_row = single_symbol_data.loc[single_symbol_data['Low'].idxmin()]  # Get the row with the minimum 'Low' value
-            max_return_row = single_symbol_data.loc[single_symbol_data['High'].idxmax()]  # Get the row with the maximum 'High' value
-            
-            company_name = single_symbol_data['Company_Name'].values[0]  # Get the Company_Name from the dataset
-            
-            st.write(f"For the dates {start_date} to {end_date}, {company_name} recorded its:") 
-            st.write(f"Lowest trading price: ${min_return_row['Low']:.2f} on {min_return_row['Datetime'].strftime('%A, %B %d at %H:%M')}")
-            st.write(f"Highest trading price: ${max_return_row['High']:.2f} on {max_return_row['Datetime'].strftime('%A, %B %d at %H:%M')}")
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
-
         
 # Function to display the time series chart for selected tickers using Plotly
 def display_time_series_chart(symbol_data, selected_symbols, start_date, end_date):
@@ -100,6 +195,30 @@ def display_time_series_chart(symbol_data, selected_symbols, start_date, end_dat
                             line=dict(color=color_mapping[symbol], width=2)
                         )
                     )
+                
+                # Add annotations for highest and lowest trading prices
+                min_return_row = symbol_data.loc[symbol_data['Low'].idxmin()]  # Get the row with the minimum 'Low' value
+                max_return_row = symbol_data.loc[symbol_data['High'].idxmax()]  # Get the row with the maximum 'High' value
+                
+                fig.add_annotation(
+                    x=min_return_row['Datetime'],
+                    y=min_return_row['Low'],
+                    text=f"Lowest: ${min_return_row['Low']:.2f}",
+                    showarrow=False,
+                    arrowhead=4,
+                    ax=0,
+                    ay=-40
+                )
+                
+                fig.add_annotation(
+                    x=max_return_row['Datetime'],
+                    y=max_return_row['High'],
+                    text=f"Highest: ${max_return_row['High']:.2f}",
+                    showarrow=False,
+                    arrowhead=4,
+                    ax=0,
+                    ay=40
+                )
             
             # Set chart title
             fig.update_layout(
@@ -119,7 +238,7 @@ st.write("""
 An interactive analysis of S&P 500 companies, allowing users to view and download historical stock data, returns, 
 additional company information. The dataset provides 1 year of historical data, recorded at hourly intervals. 
 """)
-
+        
 url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
 tickers = fetch_sp500_data(url)
 Stocks = tickers.Symbol.to_list()
@@ -137,7 +256,8 @@ if portfolio is not None:
     # Ticker selection
     default_ticker = ['AAPL']
     selected_symbols = st.multiselect("Tickers:", filtered_portfolio['Symbol'].unique(), default=default_ticker)
-
+    
+                
     # Filter the data for the selected symbols
     symbol_data = filtered_portfolio[filtered_portfolio['Symbol'].isin(selected_symbols)]
 
@@ -145,11 +265,31 @@ if portfolio is not None:
     if selected_symbols:  # Check if at least one ticker is selected
         display_time_series_chart(symbol_data, selected_symbols, start_date, end_date)
     else:
-        st.warning("Please select at least one ticker for comparison.")
-   
-    # Call the display_high_low function here
-    display_high_low(symbol_data, selected_symbols, start_date, end_date)
-  
+        st.warning("Please select at least one ticker for comparison.") 
+
+   # ESG Data Retrieval and Display
+    esg_data_list = []
+    esg_scores = []
+    
+    for symbol in selected_symbols:
+        esg_data = get_esg_data_with_headers_and_error_handling(symbol)
+        if esg_data:
+            esg_data_list.append(esg_data)
+            esg_scores.append(esg_data.get("Total ESG risk score", None))
+    
+    # Display consolidated ESG data table
+    if esg_data_list:
+        display_esg_data_table(selected_symbols, esg_data_list)
+    
+    # Display ESG risk levels visualization for all selected tickers
+    if esg_scores:
+        display_risk_levels(selected_symbols, esg_scores)
+            
+        st.markdown("**Data Source:** [Yahoo Finance](https://finance.yahoo.com/)")
+        st.markdown("**Risk Ratings:** Conducted by [Sustainalytics](https://www.sustainalytics.com/)")
+                      
+    else:
+        st.write(f"No ESG data available for {symbol}.")
       
     # Now display the data table
     if 'Datetime' in symbol_data.columns:
