349e00622b4d016269c7a5906f353e540b97421a
YKKaya <73110464+YKKaya@users.noreply.github.com>
2023-10-13 14:07:29 +0200

    Update SP500.py

--- SP500.py
+++ SP500.py
@@ -6,7 +6,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # Function to fetch S&P 500 data
-@st.cache_data
+@st.cache
 def fetch_sp500_data(url):
     try:
         tickers = pd.read_html(url)[0]
@@ -16,24 +16,157 @@ def fetch_sp500_data(url):
         return None
 
 # Function to download stock data
-@st.cache_data
+@st.cache
 def download_stock_data(Stocks):
     try:
         Portfolio = yf.download(Stocks, period='1y', interval='1h')
-        Portfolio = Portfolio.stack(level=0).rename_axis(['Date', 'Symbol']).reset_index(level=1)
         return Portfolio
     except Exception as e:
         st.error(f"Error downloading stock data: {e}")
         return None
-
-# Function to extract ESG
-@st.cache_data
+        
+# Function to extract esg data        
+@st.cache
 def get_esg_data_with_headers_and_error_handling(ticker):
-    # ... [same code as before] ...
+    url = f"https://uk.finance.yahoo.com/quote/{ticker}/sustainability?p={ticker}"
+    response = requests.get(url, headers=headers)
 
+    if response.status_code != 200:
+        print(f"Failed to fetch data for {ticker}. Status code: {response.status_code}")
+        return None
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    result = {}
+
+    try:
+        total_esg_risk_score = soup.find("div", {"class": "Fz(36px) Fw(600) D(ib) Mend(5px)"}).text
+        result["Total ESG risk score"] = float(total_esg_risk_score)
+    except:
+        result["Total ESG risk score"] = None
+
+    try:
+        nth_percentile = soup.find("div", {"class": "D(ib) Fz(12px) Fw(n) Mstart(2px)"}).text
+        result["n'th percentile"] = nth_percentile
+    except:
+        result["n'th percentile"] = None
+
+    scores = soup.find_all("div", {"class": "D(ib) Fz(23px) smartphone_Fz(22px) Fw(600)"})
+    try:
+        result["Environment risk score"] = float(scores[0].text)
+    except:
+        result["Environment risk score"] = None
+
+    try:
+        result["Social risk score"] = float(scores[1].text)
+    except:
+        result["Social risk score"] = None
+
+    try:
+        result["Governance risk score"] = float(scores[2].text)
+    except:
+        result["Governance risk score"] = None
+
+    try:
+        controversy_level = soup.find("div", {"class": "D(ib) Fz(36px) Fw(500)"}).text
+        result["Controversy level"] = int(controversy_level)
+    except:
+        result["Controversy level"] = None
+
+    return result
+    
+# Function to process data
+def process_data(Portfolio):
+    try:
+        portfolio = Portfolio.stack().reset_index().rename(index=str, columns={"level_1": "Symbol", "level_0": "Datetime"})
+        portfolio['Return'] = (portfolio['Close'] - portfolio['Open']) / portfolio['Open']
+        return portfolio
+    except Exception as e:
+        st.error(f"Error processing data: {e}")
+        return None
+
+# Function to merge additional info
+def merge_additional_info(portfolio, tickers):
+    try:
+        company_info = tickers[['Symbol', 'Security', 'GICS Sector', 'GICS Sub-Industry', 'Headquarters Location', 'Date added', 'Founded']]
+        company_info.columns = ['Symbol', 'Company_Name', 'Industry', 'Sub_Industry', 'Headquarters_Location', 'Date_Added', 'Founded']
+        portfolio = pd.merge(portfolio, company_info, on='Symbol', how='left')
+        return portfolio
+    except Exception as e:
+        return None
+        
+# Function to display high and low return text
+def display_high_low(symbol_data, selected_symbols, start_date, end_date):
+    try:
+        for symbol in selected_symbols:
+            single_symbol_data = symbol_data[symbol_data['Symbol'] == symbol]
+            single_symbol_data['Datetime'] = pd.to_datetime(single_symbol_data['Datetime'])  # Ensure Datetime is in datetime format
+            if single_symbol_data.empty:
+                st.error(f"No data available for {symbol} in the selected date range.")
+                continue
+            min_return_row = single_symbol_data.loc[single_symbol_data['Low'].idxmin()]  # Get the row with the minimum 'Low' value
+            max_return_row = single_symbol_data.loc[single_symbol_data['High'].idxmax()]  # Get the row with the maximum 'High' value
+            
+            company_name = single_symbol_data['Company_Name'].values[0]  # Get the Company_Name from the dataset
+            
+            st.write(f"For the dates {start_date} to {end_date}, {company_name} recorded its:") 
+            st.write(f"Lowest trading price: ${min_return_row['Low']:.2f} on {min_return_row['Datetime'].strftime('%A, %B %d at %H:%M')}")
+            st.write(f"Highest trading price: ${max_return_row['High']:.2f} on {max_return_row['Datetime'].strftime('%A, %B %d at %H:%M')}")
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+
+# Function to display the time series chart for selected tickers using Plotly
+def display_time_series_chart(symbol_data, selected_symbols, start_date, end_date):
+    try:
+        filtered_data = symbol_data[
+            (symbol_data['Symbol'].isin(selected_symbols)) &
+            (symbol_data['Datetime'].dt.date >= start_date) &
+            (symbol_data['Datetime'].dt.date <= end_date)
+        ]
+
+        if filtered_data.empty:
+            st.error("No data available for the selected symbols in the selected date range.")
+        else:
+            selected_tickers = ', '.join(selected_symbols)  # Join selected tickers with commas
+            
+            # Create a Plotly line chart
+            fig = go.Figure()  # Create a new Plotly figure
+            
+            # Customize the chart with explicit light colors
+            light_colors = ['#FF5733', '#FFBD33', '#33FF57', '#339CFF', '#FF33D1']  # Light colors
+            color_mapping = {symbol: light_colors[i % len(light_colors)] for i, symbol in enumerate(selected_symbols)}
+            
+            for symbol in selected_symbols:
+                symbol_data = filtered_data[filtered_data['Symbol'] == symbol]
+                # Add trace only once for each unique symbol
+                if symbol not in fig.data:
+                    fig.add_trace(
+                        go.Scatter(
+                            x=symbol_data['Datetime'],
+                            y=symbol_data['High'],
+                            mode='lines',
+                            name=symbol,
+                            line=dict(color=color_mapping[symbol], width=2)
+                        )
+                    )
+            
+            # Set chart title
+            fig.update_layout(
+                title=f"Time Series Chart for {selected_tickers} Tickers",
+                xaxis_title="Date",
+                yaxis_title="Highest Price"
+            )
+            
+            # Show the chart
+            st.plotly_chart(fig)
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+        
 # Main part of the code
-st.title("S&P 500 Companies Hourly Returns and ESG Data")
-
+st.title("S&P 500 Companies Hourly Returns")
+st.write("""
+An interactive analysis of S&P 500 companies, allowing users to view and download historical stock data, returns, 
+additional company information. The dataset provides 1 year of historical data, recorded at hourly intervals. 
+""")
 # ESG Data Retrieval
 st.write("### Retrieve ESG Data")
 user_input_ticker = st.text_input("Enter a ticker symbol for ESG data (e.g., AAPL):")
@@ -44,33 +177,29 @@ if user_input_ticker:
         st.write(esg_data)
     else:
         st.write(f"No ESG data available for {user_input_ticker}.")
-
+        
 url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
 tickers = fetch_sp500_data(url)
 Stocks = tickers.Symbol.to_list()
 Portfolio = download_stock_data(Stocks)
+portfolio = process_data(Portfolio)
+portfolio = merge_additional_info(portfolio, tickers)
 
-# Date range selection
-st.write("Select Date Range:")
-start_date = st.date_input("Start Date", value=datetime.now() - timedelta(days=30), max_value=datetime.now())
-end_date = st.date_input("End Date", value=datetime.now(), max_value=datetime.now())
-filtered_portfolio = Portfolio[(Portfolio['Date'].dt.date >= start_date) & (Portfolio['Date'].dt.date <= end_date)]
+if portfolio is not None:
+    # Date range selection
+    st.write("Select Date Range:")
+    start_date = st.date_input("Start Date", value=datetime.now() - timedelta(days=30), max_value=datetime.now())
+    end_date = st.date_input("End Date", value=datetime.now(), max_value=datetime.now())
+    filtered_portfolio = portfolio[(portfolio['Datetime'].dt.date >= start_date) & (portfolio['Datetime'].dt.date <= end_date)]
 
-# Ticker selection
-default_ticker = ['AAPL']
-selected_symbols = st.multiselect("Tickers:", filtered_portfolio['Symbol'].unique(), default=default_ticker)
+    # Ticker selection
+    default_ticker = ['AAPL']
+    selected_symbols = st.multiselect("Tickers:", filtered_portfolio['Symbol'].unique(), default=default_ticker)
 
-# Filter the data for the selected symbols
-symbol_data = filtered_portfolio[filtered_portfolio['Symbol'].isin(selected_symbols)]
+    # Filter the data for the selected symbols
+    symbol_data = filtered_portfolio[filtered_portfolio['Symbol'].isin(selected_symbols)]
 
-# Display the time series chart for selected tickers
-if selected_symbols:  # Check if at least one ticker is selected
-    # ... [rest of your Streamlit app code here] ...
-else:
-    st.warning("Please select at least one ticker for comparison.")
-
-
-    # Display the time series chart for selected tickers
+   # Display the time series chart for selected tickers
     if selected_symbols:  # Check if at least one ticker is selected
         display_time_series_chart(symbol_data, selected_symbols, start_date, end_date)
     else:
@@ -78,6 +207,7 @@ else:
    
     # Call the display_high_low function here
     display_high_low(symbol_data, selected_symbols, start_date, end_date)
+  
       
     # Now display the data table
     if 'Datetime' in symbol_data.columns:
